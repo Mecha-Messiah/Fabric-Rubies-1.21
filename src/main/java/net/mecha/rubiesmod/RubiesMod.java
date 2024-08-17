fddf06af024d8c83e99c9e0a5517f4f36f3b4dff
Mecha-Messiah <139387048+Mecha-Messiah@users.noreply.github.com>
2024-08-17 14:42:41 -0400

    Initial commit

---
+++ src/main/java/net/mecha/rubiesmod/RubiesMod.java
@@ -0,0 +1,16 @@
+package net.mecha.rubiesmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RubiesMod implements ModInitializer {
+	public static final String MOD_ID = "rubiesmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mecha/rubiesmod/RubiesModClient.java
@@ -0,0 +1,10 @@
+package net.mecha.rubiesmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubiesModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
