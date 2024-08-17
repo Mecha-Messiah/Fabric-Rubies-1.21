e08d68518229325c14159f60e605c48b420d0c03
Mecha-Messiah <139387048+Mecha-Messiah@users.noreply.github.com>
2024-08-17 14:46:02 -0400

    Added notes

--- src/main/java/net/mecha/rubiesmod/RubiesMod.java
+++ src/main/java/net/mecha/rubiesmod/RubiesMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//MOD ID STRING
 public class RubiesMod implements ModInitializer {
 	public static final String MOD_ID = "rubiesmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
--- src/main/java/net/mecha/rubiesmod/RubiesModDataGenerator.java
+++ src/main/java/net/mecha/rubiesmod/RubiesModDataGenerator.java
@@ -3,6 +3,7 @@ package net.mecha.rubiesmod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+//HOPEFULLY THIS WORKS
 public class RubiesModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
