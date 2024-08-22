8a96ce4bfebff6325dd0e059bc8f11044df631e8
Mecha-Messiah <139387048+Mecha-Messiah@users.noreply.github.com>
2024-08-22 15:46:23 -0400

    Important changes

--- src/main/java/net/mecha/rubiesmod/block/ModBlocks.java
+++ src/main/java/net/mecha/rubiesmod/block/ModBlocks.java
@@ -3,17 +3,26 @@ package net.mecha.rubiesmod.block;
 import net.mecha.rubiesmod.RubiesMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
             new Block(AbstractBlock.Settings.create().strength(5f,6f)
                     .requiresTool().sounds(BlockSoundGroup.METAL)));
+
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
