c23b81a022452d4527c8112a2779b2ca799286d9
Mecha-Messiah <139387048+Mecha-Messiah@users.noreply.github.com>
2024-08-25 18:19:44 -0400

    Important changes

---
+++ src/main/java/net/mecha/rubiesmod/item/custom/ArchitectWandItem.java
@@ -0,0 +1,11 @@
+package net.mecha.rubiesmod.item.custom;
+
+import net.minecraft.item.Item;
+
+public class ArchitectWandItem extends Item {
+    public ArchitectWandItem(Settings settings) {
+        super(settings);
+    }
+
+
+}
